6b6daf5b9e9e8102c6c549cd074942d46b08dfe4
Imposition. <94856897+lmposition@users.noreply.github.com>
2025-08-16 21:32:24 -1000

    Main

---
+++ src/main/java/net/mcreator/axisgui/client/screens/AxisNotificationZoneOverlay.java
@@ -0,0 +1,56 @@
+
+package net.mcreator.axisgui.client.screens;
+
+import org.checkerframework.checker.units.qual.h;
+
+import net.neoforged.neoforge.client.event.RenderGuiEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.bus.api.EventPriority;
+import net.neoforged.api.distmarker.Dist;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.network.chat.Component;
+import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.client.Minecraft;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.platform.GlStateManager;
+
+@EventBusSubscriber({Dist.CLIENT})
+public class AxisNotificationZoneOverlay {
+	@SubscribeEvent(priority = EventPriority.HIGH)
+	public static void eventHandler(RenderGuiEvent.Pre event) {
+		int w = event.getGuiGraphics().guiWidth();
+		int h = event.getGuiGraphics().guiHeight();
+		Level world = null;
+		double x = 0;
+		double y = 0;
+		double z = 0;
+		Player entity = Minecraft.getInstance().player;
+		if (entity != null) {
+			world = entity.level();
+			x = entity.getX();
+			y = entity.getY();
+			z = entity.getZ();
+		}
+		RenderSystem.disableDepthTest();
+		RenderSystem.depthMask(false);
+		RenderSystem.enableBlend();
+		RenderSystem.setShader(GameRenderer::getPositionTexShader);
+		RenderSystem.blendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
+		RenderSystem.setShaderColor(1, 1, 1, 1);
+		if (true) {
+			event.getGuiGraphics().blit(new ResourceLocation("axis_gui:textures/screens/axisnotif.png"), w - 222, 3, 0, 0, 225, 44, 225, 44);
+
+			event.getGuiGraphics().drawString(Minecraft.getInstance().font, Component.translatable("gui.axis_gui.axis_notification_zone.label_caelora_village"), w / 2 + 66, h / 2 + -95, -7473, false);
+		}
+		RenderSystem.depthMask(true);
+		RenderSystem.defaultBlendFunc();
+		RenderSystem.enableDepthTest();
+		RenderSystem.disableBlend();
+		RenderSystem.setShaderColor(1, 1, 1, 1);
+	}
+}
